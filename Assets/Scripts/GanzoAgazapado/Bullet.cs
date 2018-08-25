512a4d65c2f3c3f7d12c95614746488b2bca1e1a
Row <rodrigo.rearden@etermax.com>
2018-08-25 15:28:02 -0300

    Destroy enemies and bullets

---
+++ Assets/Scripts/GanzoAgazapado/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using GanzoAgazapado.Enemies;
+using UnityEngine;
+
+namespace GanzoAgazapado
+{
+    public class Bullet : MonoBehaviour
+    {
+        public Rigidbody body;
+
+	    void OnCollisionEnter(Collision other)
+	    {
+		    var enemy = other.gameObject.GetComponent<Enemy>();
+
+		    if (enemy != null)
+		    	Destroy(gameObject);
+	    }
+    }
+}
--- Assets/Scripts/GanzoAgazapado/Enemies/Enemy.cs
+++ Assets/Scripts/GanzoAgazapado/Enemies/Enemy.cs
@@ -10,5 +10,14 @@ namespace GanzoAgazapado.Enemies
 		static Quaternion Rotation(Vector3 forward) => Quaternion.LookRotation(forward, Vector3.up);
 
 		static Vector3 Forward(Vector3 position, Transform player) => (player.position - position).normalized;
+
+		void OnCollisionEnter(Collision other)
+		{
+			var bullet = other.gameObject.GetComponent<Bullet>();
+
+			if (bullet != null) {
+				Destroy(gameObject);
+			}
+		}
 	}
 }
