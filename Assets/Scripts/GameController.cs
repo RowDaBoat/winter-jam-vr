0510722c7ee2e6884f9183cd90925c48aecf4b1b
Ruben Garat <ruben01@gmail.com>
2018-08-25 15:27:05 -0300

    fixed scene split

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,6 +9,12 @@ namespace GanzoAgazapado
         void Awake()
         {
             var scene = VRScene.Instance;
+            Invoke("InitializeGame", 0.1f);
+        }
+
+        void InitializeGame()
+        {
+            var scene = VRScene.Instance;
             scene.player.transform.position = playerStart.transform.position;
             scene.player.transform.forward = playerStart.transform.forward;
         }
