f5d43e8a7d10e1fa9a421e91aa20d230fddd30f8
Row <rodrigo.rearden@etermax.com>
2018-08-25 20:13:00 -0300

    Shoot and reload

---
+++ Assets/Scripts/GanzoAgazapado/Gun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using static GanzoAgazapado.CoroutineUtils;
+
+namespace GanzoAgazapado
+{
+	public class Gun : MonoBehaviour
+	{
+		public int totalRound = 4;
+		public float reloadTime = 3f;
+		public ShootLogic shootLogic;
+
+		int round;
+		
+		public void Shoot()
+		{
+			round--;
+			shootLogic.Fire();
+		}
+		
+		void Start()
+		{
+			round = 4;
+			StartCoroutine(Rounds());
+		}
+
+		IEnumerator Rounds() => Wait(() => round == 0)
+			.Then(Do(() => Debug.Log("Reloading...")))
+			.Then(Wait(reloadTime))
+			.Then(Reload())
+			.Then(Do(() => Debug.Log("Reloaded!")))
+			.Repeat();
+
+		IEnumerator Reload()
+		{
+			round = totalRound;
+			yield return 0;
+		}
+	}
+}
