9fd290017813dfa535cd15f688c83567d130e1d7
Row <rodrigo.rearden@etermax.com>
2018-08-25 22:38:30 -0300

    Add wooooooooooommmmmm

--- Assets/Scripts/GanzoAgazapado/GameLoop/Loop.cs
+++ Assets/Scripts/GanzoAgazapado/GameLoop/Loop.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using JetBrains.Annotations;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,18 +13,43 @@ namespace GanzoAgazapado.GameLoop
 		public TextMeshProUGUI screenText;
 		public Image initialDimmer;
 		public Image redDimmer;
+		public AudioSource music;
+		public AudioSource loseSound;
 
 		public event Action OnReady = delegate {};
 		public event Action OnStop = delegate {};
 
 		int score = 0;
+		bool finished;
 
 		public void StartGame() => StartCoroutine(StartRoutine());
 		
 		public void Lose()
 		{
+			if (finished)
+				return;
+
+			finished = true;
 			ShowDimmer(redDimmer, "Ded Ganz!\n\nScore: " + score);
 			OnStop();
+			StartCoroutine(FadeMusic(0.25f));
+		}
+
+		IEnumerator FadeMusic(float time)
+		{
+			var t = 0f;
+
+			loseSound.Play();
+			
+			while (t < time) {
+				music.volume = 1f - t / time;
+				loseSound.volume = t / time;
+				t += Time.deltaTime;
+
+				yield return 0;
+			}
+
+			music.volume = 0;
 		}
 
 		public void AddScore() => score++;
@@ -37,6 +63,7 @@ namespace GanzoAgazapado.GameLoop
 			.Then(Do(() => ShowText("1")))
 			.Then(Wait(1))
 			.Then(Do(() => ShowText("Agazapated Ganz!")))
+			.Then(Do(() => music.Play()))
 			.Then(Wait(1))
 			.Then(Do(() => HideDimmer(initialDimmer)))
 			.Then(Do(() => OnReady()));
